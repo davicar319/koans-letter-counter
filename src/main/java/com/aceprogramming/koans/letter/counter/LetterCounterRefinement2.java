6eb77750060c1bf8fd7e47292504bfb236d5ab94
DaveCarlson <dave@ace-programming.com>
2020-04-23 15:14:16 -0500

    Refinement using Java Streams API.

--- src/main/java/com/aceprogramming/koans/letter/counter/LetterCounterRefinement2.java
+++ src/main/java/com/aceprogramming/koans/letter/counter/LetterCounterRefinement2.java
@@ -3,16 +3,11 @@ package com.aceprogramming.koans.letter.counter;
 import java.util.HashMap;
 import java.util.Map;
 
-public class LetterCounterRefinement2 {
+final class LetterCounterRefinement2 implements LetterCounter{
     LetterCounterRefinement2() {
     }
 
     public Map<Character, Integer> countUsage(String inputString) {
-        return inputString == null ? countLetters("") :
-                countLetters(inputString);
-    }
-
-    private static Map<Character, Integer> countLetters(String inputString) {
         HashMap<Character, Integer> result = new HashMap<>();
         for (int i = 0; i < inputString.length(); i++) {
             char c = inputString.charAt(i);
